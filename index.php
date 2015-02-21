f6b40f63f1d1bbe456de9f0fe21c9e524f4b3c6c
Nicholas Hoffmann <nmjbhoffmann@gmail.com>
2015-02-21 03:08:47 +0200

    Fixed issue when a file is in the www directory Last update till I move over to automatic subdomain management with dnsmasq which will prevent the need to edit the hostfile and creating vhosts on from the site

--- index.php
+++ index.php
@@ -7,15 +7,20 @@
 </head>
 <body>
 <div class="sidebar">
-	<div class="side_title">TEST</div>
+	<div class="side_title"><?php echo $_SERVER['SERVER_NAME']?></div>
 	<div class="rule"></div>
 	<div class="side_head">Applications</div>
 	<div class="side_container">
 		<a href="<?=$base_url;?>phpmyadmin">PHPMyadmin</a>
 	</div>
+	<div class="side_head">Add Site</div>
+	<form name="input" action="" method="post" class="side-input">
+		<input type="text" name="site">
+		<?php if (isset($_POST['site'])) { create_site($_POST['site'],'.'.$_SERVER['SERVER_NAME']); } ?>
+	</form> 
 </div>
 <div class="content">
-<?php list_sites($exclude_list,'.'.$_SERVER['SERVER_NAME'],"file_list"); ?>
+<?php list_sites($exclude_list,'.'.'kirastation1',"file_list"); ?>
 
 
 
