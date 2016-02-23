a60857e155318f4dcdcf98ea9c8e45a15554e167
Nicholas Hoffmann <nmjbhoffmann@gmail.com>
2016-02-23 04:52:42 +0200

    Added various changes to the script that I've been user over the past year or so

--- functions.php
+++ functions.php
@@ -1,75 +1,109 @@
 <?php
 function localhost_redirect($url){
-	if (($_SERVER['SERVER_NAME'] == 'localhost') && $url !="")
-	{
-		header('Location: '.$url);
-	}
+    if (($_SERVER['SERVER_NAME'] == 'localhost') && $url !="")
+    {
+        header('Location: '.$url);
+    }
 }
 
 function get_folder_size($path){
-	$path = "'".$path."'";
-	$io   = popen ( '/usr/bin/du -h -s '.$path, 'r' );
-	$size = fgets ( $io, 4096);
-	$size = substr ( $size, 0, strpos ( $size, "\t" ) );
+    $path = "'".$path."'";
+    $io   = popen ( '/usr/bin/du -h -s '.$path, 'r' );
+    $size = fgets ( $io, 4096);
+    $size = substr ( $size, 0, strpos ( $size, "\t" ) );
     pclose ( $io );
     return $size;
 }
 
-function list_sites($exclude_list = array(".",".."),$path,$submode = false,$domain_name="",$class=""){
-	$dir_handle = @opendir($path) or die("Invalid Directory");
+function update_host_file($hosts = 'localhost',$line_number){
 
-	while (false !== ($folder = readdir($dir_handle))) {
-	if(in_array($folder, $exclude_list))
-	continue;
+    $f = fopen('/etc/hosts', 'r');
+    $data = '';
+    for ($i = 1; ($line = fgets($f)) !== false; $i++) {
+        if ($i == $line_number){
+            $line = "127.0.0.1   ".$hosts." #Generated Automatically by Localhost Script".PHP_EOL;
+        }else {
+            $line = $line;
+        }
 
-		//Loop through the projects to see what files they contain
-		if (is_dir($path.'/'.$folder)) { // Check if it is a folder, since it's pointless having a subdomain point to a file
+        $data = $data.$line;
+    }
+    $handle = fopen('/etc/hosts', 'w') or
+    die(
+        'Cannot write to your hosts file, please check apache has write access to your /etc/hosts file.<br/>'.
+        'Note doing so is a security risk and only do this if you know what you are doing, or set $hostfile_update=0 and update your hostfile manually.'
+    );
 
-			$project_path  = @opendir($path.'/'.$folder);
-			$project_files = array('');
+    fwrite($handle, $data);
+    fclose($f);
 
-			while ($pfile = readdir($project_path)) {
-				array_push($project_files, $pfile);
-			}
+}
 
-			closedir($project_path);
-		
+function list_sites($exclude_list = array(".",".."),$path,$submode = false,$domain_name="",$class="",$hostfile_update=0){
+    $dir_handle = @opendir($path) or die("Invalid Directory");
+    $file_list = "";
+    $host_list = "";
 
-			//Check what type of project it is based on the files inside
-			if (in_array("wp-config.php", $project_files)) {
-				$type = "Wordpress";
-			}
-			elseif (in_array("application", $project_files)) {
-				$type = "CodeIgnitor";
-			}
-			elseif (in_array("app", $project_files) && in_array("src", $project_files)) {
-				$type = "Symfony";
-			}
-			elseif (in_array("backend", $project_files) && in_array("console", $project_files)) {
-				$type = "Yii";
-			}
-			else {
-				$type = "Custom Project";
-			}
+    while (false !== ($folder = readdir($dir_handle))) {
+    if(in_array($folder, $exclude_list))
+    continue;
+
+        //Loop through the projects to see what files they contain
+        if (is_dir($path.'/'.$folder)) { // Check if it is a folder, since it's pointless having a subdomain point to a file
+
+            $project_path  = @opendir($path.'/'.$folder);
+            $project_files = array('');
+
+            while ($pfile = readdir($project_path)) {
+                array_push($project_files, $pfile);
+            }
+
+            closedir($project_path);
 
 
-			//Get Folder Size
-			$size = get_folder_size($path."/".$folder."/");
-			//Make some pretty File names
-			$file_title = str_replace("_", " ", $folder);
-			$file_title = ucwords(strtolower($file_title));
-			if ($submode == true) {
-				echo "<li class='".$class." ".$type."'><a href='http://".$folder.".".$domain_name."'>".$file_title."</a><small class='tags ".$type."'>".$type." <div class='tag-size'>= ".$size."</div></small></li>";
-			}
-			else{
-				echo "<li class='".$class." ".$type."'><a href='http://".$domain_name."/".$folder."'>".$file_title."</a><small class='tags ".$type."'>".$type." <div class='tag-size'>= ".$size."</div></small></li>";
-			}
-			
-		}
-		
-		
-	}
-	closedir($dir_handle);
+            //Check what type of project it is based on the files inside
+            if (in_array("wp-config.php", $project_files)) {
+                $type = "Wordpress";
+            }
+            elseif (in_array("application", $project_files)) {
+                $type = "CodeIgnitor";
+            }
+            elseif (in_array("app", $project_files) && in_array("src", $project_files)) {
+                $type = "Symfony";
+            }
+            elseif (in_array("backend", $project_files) && in_array("console", $project_files)) {
+                $type = "Yii";
+            }
+            else {
+                $type = "Custom Project";
+            }
+
+            //Get Folder Size, This will impact performace of the site when you have folders with milions of files.
+            $size = get_folder_size($path."/".$folder."/");
+            //Make some pretty File names
+            $file_title = str_replace("_", " ", $folder);
+            $file_title = ucwords(strtolower($file_title));
+            if ($submode == true) {
+                $file_list = "<li class='".$class." ".$type."'><a href='http://".$folder.".".$domain_name."'>".$file_title."</a><small class='tags ".$type."'>".$type." <div class='tag-size'>= ".$size."</div></small></li>".$file_list;
+            }
+            else{
+                $file_list =  "<li class='".$class." ".$type."'><a href='http://".$domain_name."/".$folder."'>".$file_title."</a><small class='tags ".$type."'>".$type." <div class='tag-size'>= ".$size."</div></small></li>".$file_list;
+
+            }
+
+            //This is used to update the hosts file
+            $host_list = $folder.".".$domain_name." ".$host_list;
+        }
+
+
+    }
+    closedir($dir_handle);
+    if ($hostfile_update != 0 && $submode=true) {
+        update_host_file($host_list,$hostfile_update);
+    }
+
+
+    return $file_list;
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,37 +1,43 @@
 <!DOCTYPE html>
 <html>
 <?php
-	//Stuff to configure
-	$path         = "/var/www";													//Path to your www directory
-	$base_url     = 'http://'.$_SERVER['SERVER_NAME'].'/'; 						//Your site URL (Normally won't need to change this)
-	$exclude_list = array(".","..","index.php","functions.php","style.css");	//list of folders you would like to exclude (default assumes you just through this project into your main www folder)
-	$submode      = false; 														//If you want to list folders as subdomain links eg: test.localhost instead of localhost/test
-	$domain_name  = $_SERVER['SERVER_NAME'];									//name of you computer, either localhost or your hostname (Normally won't need to change this)
-	$class        = "file_list"; 												//A class name you would like your list to have, normally no change needed
+    //Stuff to configure
+    $path            = "/var/www";                                              //Path to your www directory
+    $base_url        = 'http://'.$_SERVER['SERVER_NAME'].'/';                   //Your site URL (Normally won't need to change this)
+    $exclude_list    = array(".","..","index.php","functions.php","style.css"); //list of folders you would like to exclude (default assumes you just through this project into your main www folder)
 
-	$redirect     = "";	//Don't set if you use http://localhost					//Makes sure you're on the right URL for people who use vhosts and set it up for their computer name 
-																				//instead of localhost but keep typing localhost into the browser (like me),
+    $submode         = false;                                                   //If you want to list folders as subdomain links eg: test.localhost instead of localhost/test
+    $hostfile_update = 0; //0 to disable                                        //IMPORTANT - This will attempt to update your hostfile if it has permission to do so (Normally the user www-data)
+                                                                                //If you want to update your hosts file on load, you need to specifiy which line number of the hosts file to update
+
+    $domain_name     = $_SERVER['SERVER_NAME'];                                 //Name of your computer, either localhost or your hostname (Normally won't need to change this)
+    $class           = "file_list";                                             //A class name you would like your list to have, normally no change needed
+
+
+    //Makes sure you're on the right URL for people who use vhosts and set up for their computer name instead of localhost, but keep typing localhost into the browser (like me).
+    $redirect        = "";  //Don't set if you use http://localhost
+
 ?>
 
 <?php
-	include('functions.php');
-	localhost_redirect($redirect);
+    include('functions.php');
+    localhost_redirect($redirect);
 ?>
 
 <head>
-	<title><?php echo $domain_name;?></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <title><?php echo $domain_name;?></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <div class="sidebar">
-	<div class="side_title"><?php echo $domain_name;?></div>
-	<div class="rule"></div>
-	<div class="side_head">Applications</div>
-	<div class="side_container">
-		<a href="<?php echo $base_url; ?>phpmyadmin">PHPMyadmin</a>
-	</div>
+    <div class="side_title">My Projects</div>
+    <div class="rule"></div>
+    <div class="side_head">Applications</div>
+    <div class="side_container">
+        <a href="#">PHPMyadmin</a>
+    </div>
 </div>
-	<div class="content"> <?php list_sites($exclude_list,$path,$submode,$domain_name,$class); ?>
+    <div class="content"> <?php echo list_sites($exclude_list,$path,$submode,$domain_name,$class,$hostfile_update); ?>
 </div>
 </body>
 </html>
